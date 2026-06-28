82c73d3cbb68c0f6b4c188e72033df2955b1ade7
RhenCloud <i@rhen.cloud>
2026-06-28 22:46:39 +0800

    feat: Add keytao customization support

--- modules/home/desktop/fcitx5/default.nix
+++ modules/home/desktop/fcitx5/default.nix
@@ -1,36 +1,174 @@
-{ ... }:
-
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.rhencloud.fcitx5;
+in
 {
-  # i18n.inputMethod = {
-  #   enable = true;
-  #   type = "fcitx5";
-  #   fcitx5 = {
-  #     waylandFrontend = true;
-  #     addons = with pkgs; [
-  #       qt6Packages.fcitx5-configtool
-  #       qt6Packages.fcitx5-chinese-addons
-  #       fcitx5-gtk
-  #       librime-octagram
-  #       librime-lua
-  #       (fcitx5-rime.override {
-  #         rimeDataPkgs = [
-  #           rime-data
-  #         ];
-  #       })
-  #     ];
-  #   };
-  # };
+  options.rhencloud.fcitx5 = {
+    enable = lib.mkEnableOption "fcitx5 and rime custom config";
 
+    extraRimeConfig = lib.mkOption {
+      type = lib.types submodule {
+        imports = [ (lib.types.submodule {
+          freeformType = lib.types.attrs;
+        }) ];
+        description = "Extra Rime config values for keytao schema";
+      };
+      default = { };
+      example = {
+        punctuator = { };
+        switches = [ ];
+      };
+      description = "Additional Rime configuration values";
+    };
 
-  # 仅管理 fcitx5 主题配置，不接管整个目录以免干扰 rime
-  home.file.".local/share/fcitx5/themes" = {
-    source = ./fcitx5/themes;
-    recursive = true;
+    extraKeyTaoConfig = lib.mkOption {
+      type = lib.types submodule {
+        imports = [ (lib.types.submodule {
+          freeformType = lib.types.attrs;
+        }) ];
+        description = "Extra Keyt ao specific config values";
+      };
+      default = { };
+      example = {
+        punctuator = { };
+        ascii_composer = { };
+      };
+      description = "Additional keyt ao configuration values";
+    };
+
+    extraDictFiles = lib.mkOption {
+      type = lib.types.listOf lib.types.attrsOf lib.types.path;
+      default = [ ];
+      example = [ {
+        name = "custom";
+        path = pkgs.writeText "custom.dict.yaml" ''
+          custom 词
+        '';
+      } ];
+      description = "Extra custom dictionary files to install";
+    };
+
+    extraLuaFiles = lib.mkOption {
+      type = lib.types.listOf lib.types.submodule (
+        lib.types.submodule (
+          { config, name, ... }: {
+            options.name = lib.mkOption {
+              type = lib.types.str;
+              description = "Name (filename) for the Lua file";
+            };
+            options.source = lib.mkOption {
+              type = lib.types.path;
+              description = "Path to the Lua file content directory";
+            };
+          }
+        )
+      );
+      default = [ ];
+      example = [ {
+        name = "emoji_filter.lua";
+        source = ./reduce_emoji_filter.lua;
+      } ];
+      description = "Extra Lua filter scripts to install";
+    };
+
+    extraAssortedFiles = lib.mkOption {
+      type = lib.types.listOf lib.types.attrsOf lib.types.path;
+      default = [ ];
+      example = [ {
+        name = "default.custom.yaml";
+        path = ./rime-config/default.custom.yaml;
+      } ];
+      description = "Extra Rime config (YAML) files to install";
+    };
+
+    extraUserPhrases = lib.mkOption {
+      type = lib.types.lines;
+      default = "";
+      example = ""
+        "customa  aaaa
+        customb  bbbb
+        customc  cccc
+      "";
+      description = "Extra user phrases (word code)";
+    };
+
+    extraYamlFiles = lib.mkOption {
+      type = lib.types.listOf lib.types.attrsOf lib.types.path;
+      default = [ ];
+      example = [ {
+        name = "keytao.extended.dict.yaml";
+        path = ./rime-config/extended.dict.yaml;
+      } ];
+      description = "Extra YAML files to install";
+    };
+
+    extraUserDictPackages = lib.mkOption {
+      type = lib.types.listOf lib.types.path;
+      default = [ ];
+      example = [ pkgs.ice2keytao ];
+      description = "Additional custom dictionary packages to install";
+    };
+
+    extraCustomPhrases = lib.mkOption {
+      type = lib.types.lines;
+      default = "";
+      example = "泠云=lgyw";
+      description = "Additional custom phrases";
+    };
   };
 
-  # 星空键道6 输入方案
-  programs.rime-keytao = {
-    enable = true;
-    rimeDataDir = ".local/share/fcitx5/rime";
+  config = lib.mkIf cfg.enable {
+    xdg.configFile."fcitx5/rime".source = "${
+      config.home.homeDirectory
+    }/.local/share/fcitx5/rime";
+
+    home.packages = with pkgs; [ rime-keytao ] ++ cfg.extraUserDictPackages;
+
+    home.activation.installRimeKeytao =
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        # Install keytao via rime addon activation
+        ${pkgs.rime-keytao}/bin/reinstall.sh
+      '';
+
+    home.activation.installRimeCustomConfig =
+      lib.hm.dag.entryAfter [ "installRimeKeytao" ] ''
+        rime_dir=$HOME/.local/share/fcitx5/rime
+
+        mkdir -p "$rime_dir"
+
+        # Install custom user phrases
+        ${pkgs.writeText "custom_phrase.txt.txt" cfg.extraCustomPhrases}
+        ${lib.optionalString (cfg.extraUserPhrases != "") 
+          ''cat > "$rime_dir/custom_phrase.txt.txt" << 'EOF'
+            ${cfg.extraUserPhrases}
+            EOF
+        ''}
+
+        # Install extra custom dictionaries
+        for file in '${lib.strings.concatStringsSep "'\'' '"'"'"' ${lib.strings.mapStrings (n: cfg.extraDictFiles.${n}.path) (lib.attrNames cfg.extraDictFiles)} ''}; do
+          dest="$rime_dir/custom"
+          dest="$dest/$(basename "$file")"
+          cp -R "$file" "$dest"
+        done
+
+        # Install extra YAML config files
+        for file in ${lib.escapeShellArgs (map (n: cfg.extraYamlFiles.${n}.path) (lib.attrNames cfg.extraYamlFiles))}; do
+          dest="$rime_dir/custom"
+          mkdir -p "$dest"
+          dest="$dest/$(basename "$file")"
+          cp -R "$file" "$dest"
+        done
+
+        # Install Lua filter scripts
+        ${lib.optionalString (cfg.extraLuaFiles != [ ]) ''
+          for filter in ${lib.escapeShellArgs (map (p: "${p.source}/${p.name}") cfg.extraLuaFiles)}; do
+            src="$filter"
+            dest="$rime_dir/custom"
+            cp -R "$src" "$dest"
+          done
+        ''}
+
+        echo "Rime custom config installed"
+      '';
   };
 }
---
+++ packages/ice2keytao/default.nix
@@ -0,0 +1,23 @@
+{ lib, stdenvNoCC, pkgs, python3 }:
+
+stdenvNoCC.mkDerivation {
+  pname = "ice2keytao";
+  version = "1.0";
+  src = ./../../scripts;
+  buildInputs = [ python3 pkgs.rime-ice pkgs.rime-keytao ];
+  buildPhase = ''
+    mkdir -p "$out/share/rime-data"
+    python3 "$src/convert-rime-ice-to-keytao.py" \
+      "${pkgs.rime-ice}/share/rime-data/cn_dicts" \
+      "${pkgs.rime-keytao}/share/rime-data/keytao.phrase.dict.yaml" \
+      "${pkgs.rime-keytao}/share/rime-data/keytao.single.dict.yaml" \
+      "$out/share/rime-data/ice2keytao.dict.yaml"
+  '';
+  installPhase = ''
+    true
+  '';
+  meta = with lib; {
+    description = "从 rime-ice base.dict.yaml 提取二字词并转换为键道6 编码";
+    license = licenses.free;
+  };
+}
