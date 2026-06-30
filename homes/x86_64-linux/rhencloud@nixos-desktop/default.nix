80927c9e53ec777b3a476b0f745fd66c73d156c0
RhenCloud <i@rhen.cloud>
2026-06-30 23:28:36 +0800

    chore: switch to keytaoUserDict for custom phrases

--- homes/x86_64-linux/rhencloud@nixos-desktop/default.nix
+++ homes/x86_64-linux/rhencloud@nixos-desktop/default.nix
@@ -6,8 +6,8 @@
   programs.home-manager.enable = true;
   programs.man.enable = false;
 
-  rhencloud.fcitx5.extraUserPhrases = ''
-    泠云	lgyw	1
-    李佳润	ljr	1
+  rhencloud.fcitx5.keytaoUserDict = ''
+    泠云	lgyw
+    李佳润	ljr
   '';
 }
